d6b46d98d6131e361addfdd2e03071c812377d52
Muhammad Abbas <muhabbaskhan812@gmail.com>
2023-08-30 17:50:22 +0500

    .

--- resturant/src/Components/Footer.tsx
+++ resturant/src/Components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer()
 {
     return(<>
-    <div className="h-12 md:h-24 p-4 lg:p-20  xl:p-40 text-red-500 flex items-center justify-between">
+    <div className="h-12 md:h-24 p-4 lg:px-20  xl:px-40 text-red-500 flex items-center justify-between">
         <Link href='/' className="font-bold text-xl">MASSIMO</Link>
         <p>ALL RIGHTS RESERVED</p>
     </div>
